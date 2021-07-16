f9f5a076d71de83f72c8e857efe5db7deb8f18e7
bedorajus18 <84348359+bedorajus18@users.noreply.github.com>
2021-07-16 13:59:42 +0200

    Symfony
    Symfony

---
+++ src/Services/HeroService.php
@@ -0,0 +1,25 @@
+<?php 
+namespace App\Services;
+
+use App\Entity\Hero;
+
+class HeroService
+{
+    private $_listeHeros = [];
+
+    function __construct()
+    {
+        $this->addHero(new Hero('Harley','MacKenzy','Harley Queen',true,'une psychiatre reconvertie en psychotique'));
+        $this->addHero(new Hero('Brice','Wayne','Batman',false,'une chauve souris avec une grosse super car'));
+        $this->addHero(new Hero('Clark','Kent','Kent',false,'un extra-terrestre louche et journaliste'));
+        $this->addHero(new Hero('Diana','Prince','Wonderwoman',false,'une amazonienne qui roske du poney'));
+    }
+    function getList()
+    {
+        return $this->_listeHeros;
+    }
+    function addHero($pHero)
+    {
+        array_push($_listeHeros,$pHero);
+    }
+}
